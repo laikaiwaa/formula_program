a533d0127ad7790828cda2d02d4927d2d2d57432
liekiewaa <33012450+laikaiwaa@users.noreply.github.com>
2020-03-18 16:57:05 +0800

    Add files via upload

---
+++ BaseMathMethod/Distribution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formulism.BaseMathMethod
+{
+    /// <summary>
+    /// 通用分佈類型
+    /// </summary>
+    public static class Distribution
+    {
+        /// <summary>
+        /// 正態分佈
+        /// </summary>
+        /// <param name="x">x,輸入值</param>
+        /// <param name="e">e,平均值</param>
+        /// <param name="sigema">sigema,方差</param>
+        /// <returns></returns>
+        public static double Normal(double x, double e, double sigema)
+        {
+            double fx =
+            1 / Math.Sqrt(2 * Math.PI) / sigema * Math.Exp(-(x - e) * (x - e) / 2 / (sigema * sigema));
+            return fx;
+        }
+    }
+}
